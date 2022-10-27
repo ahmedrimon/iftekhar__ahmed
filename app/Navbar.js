3508860b81acfb320dcf7fbad6fd2b9185cbc4a2
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-10-27 14:08:53 +0600

    hlw

---
+++ app/Navbar.js
@@ -0,0 +1,11 @@
+
+const Navbar = () => {
+     return (
+          <div>
+               <h2>Home</h2>
+               <h2>About</h2>
+          </div>
+     );
+};
+
+export default Navbar;
--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
+import Navbar from "./Navbar.js";
 
 export default function RootLayout({ children }) {
   return (
     <html>
-
+      <Navbar />
       <body>{children}</body>
     </html>
   )
