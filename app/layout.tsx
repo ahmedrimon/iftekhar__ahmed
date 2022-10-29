e53eb562d5adb3a7047fb37ae9c79aca461a9a4d
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-10-29 14:50:53 +0600

    little bit change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
-import Head from 'next/head'
-import navbar from "../components/Navbar/navbar";
+import Navbar from "../components/Navbar/navbar";
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <Head/>
+    <div>
+      <head>
+        <title>Iftekhar Ahmed</title>
+      </head>
       <body>
-        <navbar />
+        <Navbar />
         {children}
       </body>
-    </>
+    </div>
   )
 }
